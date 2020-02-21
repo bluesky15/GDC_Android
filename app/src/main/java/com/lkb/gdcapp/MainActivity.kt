70ed7fbe82371bf2cd6c6ed28e018696e222a45b
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-02-21 11:31:05 +0530

    simple toast

--- app/src/main/java/com/lkb/gdcapp/MainActivity.kt
+++ app/src/main/java/com/lkb/gdcapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.lkb.gdcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener{
+            Toast.makeText(applicationContext,"Hello",Toast.LENGTH_LONG).show()
+        }
     }
 }
